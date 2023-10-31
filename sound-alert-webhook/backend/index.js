2d0aecf24137e4a0d8133fb318882e30fd319cad
IBM-GuuTAR <tinnapop.pratheep@ibm.com>
2023-10-31 22:39:52 +0700

    add bodyParser

--- sound-alert-webhook/backend/index.js
+++ sound-alert-webhook/backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const PORT = 8080
 
 const app = express()
+
+app.use(bodyParser.json())
 
 app.get('/', (_, res) => {
     res.send({'message': 'Hello world!'}).end()
