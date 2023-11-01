df429e89e812e3ecc6e9ef3021a0ec76e9b2b61a
IBM-GuuTAR <tinnapop.pratheep@ibm.com>
2023-11-01 17:01:32 +0700

    Edit UI

--- sound-alert-webhook/web/src/App.js
+++ sound-alert-webhook/web/src/App.js
@@ -1,28 +1,61 @@
-import './App.css'
-import { ErrorAlertCard, WarningAlertCard } from './components/AlertCard'
-import { playWarningSound, playErrorSound } from './services/playSound'
-import { useAlerts } from './hooks/useAlerts'
+import { useState } from "react"
+
+import "./App.css"
+import { ErrorAlertCard, WarningAlertCard } from "./components/AlertCard"
+import { playWarningSound, playErrorSound } from "./services/playSound"
+import { useAlerts } from "./hooks/useAlerts"
 
 function App() {
   const { warningAlerts, errorAlerts } = useAlerts()
+
+  const [isEnabled, setIsEnabled] = useState(false)
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>Alert Center</h2>
-        <div className="Flex-row" style={{ gap: '16px', alignItems: 'center' }}>
-          <label>Test sound</label>
-          <button onClick={playWarningSound}>Warning</button>
-          <button onClick={playErrorSound}>Error</button>
-        </div>
-        <div className="Flex-row" style={{ gap: '16px' }}>
-          <div className="Flex-col" style={{ gap: '16px', marginTop: '32px' }}>
-            {warningAlerts.map((alert, idx) => <WarningAlertCard key={idx} {...alert} />)}
+        {isEnabled ? (
+          <>
+            <label>Receiving Alert</label>
+            <div
+              className="Flex-row"
+              style={{ gap: "16px", alignItems: "center", marginTop: "16px" }}
+            >  
+              <button onClick={playWarningSound}>Warning Sound</button>
+              <button onClick={playErrorSound}>Error Sound</button>
+            </div>
+            <div className="Flex-row" style={{ gap: "16px" }}>
+              <div
+                className="Flex-col"
+                style={{ gap: "16px", marginTop: "32px" }}
+              >
+                {warningAlerts.map((alert, idx) => (
+                  <WarningAlertCard key={idx} {...alert} />
+                ))}
+              </div>
+              <div
+                className="Flex-col"
+                style={{ gap: "16px", marginTop: "32px" }}
+              >
+                {errorAlerts.map((alert, idx) => (
+                  <ErrorAlertCard key={idx} {...alert} />
+                ))}
+              </div>
+            </div>
+          </>
+        ) : (
+          <div>
+            <p>Start receiving alert here</p>
+            <button
+              onClick={() => {
+                playWarningSound()
+                setIsEnabled(true)
+              }}
+            >
+              Let's start
+            </button>
           </div>
-          <div className="Flex-col" style={{ gap: '16px', marginTop: '32px' }}>
-            {errorAlerts.map((alert, idx) => <ErrorAlertCard key={idx} {...alert} />)}
-          </div>
-        </div>
+        )}
       </header>
     </div>
   )
